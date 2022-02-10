eda4397abf683d7a2bad68e102aba22a0bec13c4
Svoby42 <35143056+Svoby42@users.noreply.github.com>
2022-02-10 19:54:33 +0100

    recursive binary

--- src/com/company/binary/App.java
+++ src/com/company/binary/App.java
@@ -7,6 +7,24 @@ public class App {
 
         System.out.println(binarySearch(pole, 1222));
 
+        System.out.println(recursiveBinarySearch(pole, 0, pole.length-1, 844));
+
+    }
+
+    public static int recursiveBinarySearch(int[] a, int p, int r, int x){
+        System.out.println("[ " + p + "..." + r + " ]");
+        if(p > r){
+            return -1;
+        } else {
+            int q = (p+r)/2;
+            if(a[q] == x){
+                return q;
+            } else if (a[q] > x){
+                return recursiveBinarySearch(a, p, q-1, x);
+            } else {
+                return recursiveBinarySearch(a, q+1, r, x);
+            }
+        }
     }
 
     public static int binarySearch(int [] a, int x){
