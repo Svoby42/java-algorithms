ed68c67cd20ea992f01c450626b1388828e63ac4
Svoby42 <35143056+Svoby42@users.noreply.github.com>
2022-02-10 19:13:48 +0100

    recursive search

--- src/com/company/recursion/App.java
+++ src/com/company/recursion/App.java
@@ -5,14 +5,16 @@ public class App {
     public static void main(String[] args) {
         reduceByOne(10);
         System.out.println(factorial(10));
+
+        int[] pole = {1,2,3,4,15,16,69,82,97,844,1541,2054};
+
+        System.out.println(recursiveLinearSearch(pole, 0, 1541));
     }
 
     public static int factorial(int n){
         if(n <= 1){
-            System.out.println("does this happen " + n);
             return 1;
         }
-        System.out.println("hhah " + n);
         return n*factorial(n-1);
     }
 
@@ -20,7 +22,17 @@ public class App {
         if(n >= 0) {
             reduceByOne(n - 1);
         }
-        System.out.println("Completed Call: " + n);
+    }
+
+    public static int recursiveLinearSearch(int[] a, int i, int x){
+        if(i > a.length -1 ){
+            return -1;
+        }else if(a[i] == x){
+            return i;
+        }else{
+            System.out.println("index at: " + i);
+            return recursiveLinearSearch(a, i+1, x);
+        }
     }
 
 }
